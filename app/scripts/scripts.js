8d3bd10d1fc7e56516c5c6f021bb1186cc25d7e9
Agares <ekeisco@gmail.com>
2013-08-29 12:43:53 -0500

    gallery added

---
+++ app/scripts/scripts.js
@@ -0,0 +1,60 @@
+/*
+    Slider
+*/
+$(window).load(function() {
+    $('.flexslider').flexslider({
+        animation: "slide",
+        controlNav: "thumbnails"
+    });
+});
+
+
+/*
+    Filterable portfolio
+*/
+jQuery(document).ready(function() {
+    $clientsHolder = $('ul.portfolio-img');
+    $clientsClone = $clientsHolder.clone(); 
+ 
+    $('.filter-portfolio a').click(function(e) {
+        e.preventDefault();
+        $filterClass = $(this).attr('class');
+ 
+        $('.filter-portfolio a').attr('id', '');
+        $(this).attr('id', 'active-imgs');
+ 
+        if($filterClass == 'all'){
+            $filters = $clientsClone.find('li');
+        }
+        else {
+            $filters = $clientsClone.find('li[data-type~='+ $filterClass +']');
+        }
+ 
+        $clientsHolder.quicksand($filters, {duration: 700}, function() {
+            $("a[rel^='prettyPhoto']").prettyPhoto({social_tools: false});
+        });
+    });
+});
+
+
+/*
+    Pretty Photo
+*/
+jQuery(document).ready(function() {
+    $("a[rel^='prettyPhoto']").prettyPhoto({social_tools: false});
+});
+
+
+/*
+    Google maps
+*/
+jQuery(document).ready(function() {
+    var position = new google.maps.LatLng(45.067883, 7.687231);
+    $('.map').gmap({'center': position,'zoom': 15, 'disableDefaultUI':true, 'callback': function() {
+            var self = this;
+            self.addMarker({'position': this.get('map').getCenter() });	
+        }
+    }); 
+});
+
+
